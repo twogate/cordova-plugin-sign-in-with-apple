dba126e85e0f2178ec277cedc882034869c21f59
keiya <keiya_21@yahoo.co.jp>
2019-08-30 00:01:20 +0900

    fixed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cordova-plugin-sign-in-with-apple
+
+You should add `cordova-plugin-add-swift-support` plugin to your project to use this plugin.
+
+```
+cordova plugin add cordova-plugin-add-swift-support --save
+```
--- src/ios/SignInWithApple.swift
+++ src/ios/SignInWithApple.swift
@@ -1,15 +1,16 @@
 @objc(SignInWithApple) class SignInWithApple : CDVPlugin {
   func signin(command: CDVInvokedUrlCommand) {
+    print("This is SIWA Plugin!")
+
     var pluginResult = CDVPluginResult(
       status: CDVCommandStatus_ERROR
     )
 
     pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_OK,
-      messageAsString: msg
+      status: CDVCommandStatus_OK
     )
 
-    self.commandDelegate!.sendPluginResult(
+    self.commandDelegate!.send(
       pluginResult,
       callbackId: command.callbackId
     )
