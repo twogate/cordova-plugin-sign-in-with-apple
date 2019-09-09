8699709bee162ec318509baaf994e8687036f94c
keiya <keiya_21@yahoo.co.jp>
2019-09-09 14:53:05 +0900

    added typescript interface & promisified

---
+++ www/sign-in-with-apple.d.ts
@@ -0,0 +1,6 @@
+"use strict";
+
+export declare class SignInWithApple {
+  signin: (options: any, success: (winParam: any) => void, error: (error: any) => void) => void;
+  signinPromise: (options: any) => Promise<any>;
+}
--- www/sign-in-with-apple.js
+++ www/sign-in-with-apple.js
@@ -1,5 +1,15 @@
 var exec = require('cordova/exec');
 
-exports.signin = function(arg0, success, error) {
-  exec(success, error, "SignInWithApple", "signin", [arg0]);
+exports.signin = function(opt, success, error) {
+  exec(success, error, "SignInWithApple", "signin", [opt]);
+};
+
+exports.signinPromise = function(opt) {
+  return new Promise((resolve, reject) => {
+    exports.signin(opt, (result) => {
+      resolve(result);
+    }, (error) => {
+      reject(error);
+    }, params);
+  });
 };
